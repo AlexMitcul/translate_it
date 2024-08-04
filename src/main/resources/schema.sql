e73de8e5f1757361354e52d93235f489988db9af
AlexandruMitcul <alexandru.mitcul@outlook.com>
2024-08-04 21:44:05 +0100

    DataSource configuration added

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS translation_requests;
+
+create table if not exists translation_requests
+(
+    id serial
+        primary key,
+    source_text text not null,
+    translated_text text not null,
+    target_lang varchar(5) not null,
+    source_lang varchar(5) not null,
+    created_at timestamp with time zone default CURRENT_TIMESTAMP,
+    ip_address varchar(20)
+);
+
+alter table translation_requests owner to postgres;
+

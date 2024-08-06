883f64355c57fe058808bd848c0c095b70af15b5
AlexandruMitcul <alexandru.mitcul@outlook.com>
2024-08-07 00:17:02 +0100

    Last minute changes

--- docker/database/init-scripts/init.sql
+++ docker/database/init-scripts/init.sql
@@ -1,6 +1,16 @@
--- Example SQL script to create a table
-CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    username VARCHAR(20) NOT NULL
+DROP TABLE IF EXISTS translation_requests;
+
+create table if not exists translation_requests
+(
+    id serial
+        primary key,
+    source_text text not null,
+    translated_text text not null,
+    target_lang varchar(5) not null,
+    source_lang varchar(5) not null,
+    created_at timestamp with time zone default CURRENT_TIMESTAMP,
+    ip_address varchar(30)
 );
+
+alter table translation_requests owner to postgres;
+
